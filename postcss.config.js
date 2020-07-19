e8ce8431f224db9314172bf3667330dc3740036c
Jason Dippel <dippelj12@gmail.com>
2020-07-19 15:46:44 -0700

    add favicon and update under construction page to match brand colours

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,6 @@
 module.exports = (ctx) => {
   // //   Use webpack's mode as ctx.env is undefined and mode is pretty much the same
   //   const NODE_ENV = ctx.webpack.mode // https://github.com/postcss/postcss-loader/issues/368#issuecomment-518492725
-  console.log('wb mode', ctx.webpack.mode)
-  console.log('ctx env', ctx.env)
 
   return {
     parser: 'postcss-scss',
